54b7e97c12819ba96d45c990d2cb006fefac7b3d
Erik Mändmaa <erikmandmaa@gmail.com>
2026-01-06 01:34:28 +0200

    Add files via upload

---
+++ Example1.py
@@ -0,0 +1,11 @@
+# Storing grades in a class
+
+hw_grades = [98, 87, 92, 96]
+quiz_grades = [9, 6, 8]
+
+item1 = hw_grades
+item2 = quiz_grades
+item3 = quiz_grades[0]
+item4 = hw_grades[2]
+list_name = [item1, item2, item3, item4]
+print(list_name)
