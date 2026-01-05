a6403c84a2421b03caec836382d19c4599488802
Erik Mändmaa <erikmandmaa@gmail.com>
2026-01-06 01:34:39 +0200

    Add files via upload

---
+++ Example2.py
@@ -0,0 +1,7 @@
+temp = [86, 80, 82, 87, 79, 80, 82]
+ph = [7.2, 7.1, 7.0, 7.0, 7.2, 7.1]
+now_playing = ['Barbie', 'Oppenheimer', 'Talk to Me', 'Blue Beetle']
+
+data = [temp, ph, now_playing]
+
+print(data)
