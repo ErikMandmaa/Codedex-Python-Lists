1ee2132ca08d9ea5cafe365f2cc7029764cefe4f
Erik Mändmaa <erikmandmaa@gmail.com>
2026-01-06 01:36:08 +0200

    Add files via upload

---
+++ Index.py
@@ -0,0 +1,8 @@
+vowels = ['a', 'e', 'i', 'o', 'u']
+# Index:   0    1    2    3    4
+
+print(vowels[0])     # Output: a
+print(vowels[1])     # Output: e
+print(vowels[2])     # Output: i
+print(vowels[3])     # Output: o
+print(vowels[4])     # Output: u
