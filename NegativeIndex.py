9bf6252a3ddd9c4e480089884844bbe7849404a9
Erik Mändmaa <erikmandmaa@gmail.com>
2026-01-06 01:36:26 +0200

    Add files via upload

---
+++ NegativeIndex.py
@@ -0,0 +1,14 @@
+vowels = ['a', 'e', 'i', 'o', 'u']
+# Index:   0    1    2    3    4
+# Index:  -5   -4   -3   -2   -1
+
+print(vowels[-5])     # Output: a
+print(vowels[-4])     # Output: e
+print(vowels[-3])     # Output: i
+print(vowels[-2])     # Output: o
+print(vowels[-1])     # Output: u
+print(vowels[0])      # Output: a
+print(vowels[1])      # Output: e
+print(vowels[2])      # Output: i
+print(vowels[3])      # Output: o
+print(vowels[4])      # Output: u
